74347c3099359687271ba8fc44a4bdae736c68c2
Jesús Giganto Diez <Jesus.Giganto@tajamar365.com>
2018-03-10 17:13:05 +0100

    ok

---
+++ Encriptar/Controllers/CifradoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using System.Security.Cryptography;
+using Encriptar.Models;
+
+namespace Encriptar.Controllers
+{
+    public class CifradoController : Controller
+    {
+        // GET: Cifrado
+        public ActionResult CifradoHash()
+        {
+            return View();
+        }
+
+        //POST: CifradoHash
+        [HttpPost]
+        public ActionResult CifradoHash(String texto
+            , String password,String accion )
+        {
+
+
+            if (accion == "cifrar")
+            {
+                var encriptado = Cipher.Encrypt(texto, password);
+                ViewBag.Resultado = encriptado.ToString();
+            }
+            if(accion=="descifrar")
+            {
+                var encriptado = Cipher.Decrypt(texto, password);
+                ViewBag.Resultado = encriptado.ToString();
+            }
+
+            return View();
+
+
+        }
+
+    }
+}
